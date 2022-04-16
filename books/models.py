05514409a93614e079525eef43d1791359fa9fb4
sarah <sarahbaqeri777@gmail.com>
2022-04-16 18:07:49 +0430

    add new model comment

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from  django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -7,9 +8,24 @@ class Book(models.Model):
     author = models.CharField(max_length=50)
     content = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='cover/', blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[self.id])
+
+
+class Comment(models.Model):
+
+    text = models.TextField
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    datetime_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} : {self.text}'
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
